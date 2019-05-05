3b54879e994e7491562682ebbcff7e6a38bed793
fikri <f.n.wijaya1@gmail.com>
2019-05-06 00:52:46 +0700

    create interface for user and notification

---
+++ app/src/main/java/hafiz/mailtracker/Fragments/User/UserTrack.java
@@ -0,0 +1,39 @@
+package hafiz.mailtracker.Fragments.User;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hafiz.mailtracker.R;
+
+public class UserTrack extends Fragment {
+
+
+    public UserTrack() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_track, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
